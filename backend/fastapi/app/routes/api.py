ea68db90b7edcd8275d2d65f9134d463b2735584
petar-savov <p.savov@outlook.com>
2023-08-09 23:44:01 +0100

    Add more routes

--- backend/fastapi/app/routes/api.py
+++ backend/fastapi/app/routes/api.py
@@ -1,10 +1,11 @@
 from typing import List
 from fastapi import APIRouter
-from .. import store, models, schemas
-from ..database import SessionLocal, engine
+from .. import store, schemas
+from ..database import SessionLocal
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session
 from fastapi import Depends, FastAPI
+from fastapi.exceptions import HTTPException
 
 router = APIRouter()
 
@@ -17,32 +18,45 @@ def get_db():
         db.close()
 
 
+# Retrieve a list of datasets with optional skip and limit parameters
 @router.get("/datasets/", response_model=List[schemas.DatasetInDB])
 async def read_datasets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     datasets = store.get_datasets(db, skip=skip, limit=limit)
     return datasets
 
 
+# Retrieve a single dataset by its ID
 @router.get("/datasets/{dataset_id}", tags=["datasets"])
-async def read_dataset(dataset_id: str):
-    return {"message": f"This will return dataset {dataset_id}"}
+async def read_dataset(dataset_id: str, db: Session = Depends(get_db)):
+    dataset = store.get_dataset(db, dataset_id=dataset_id)
+    return dataset
 
 
-@router.post("/datasets/", tags=["datasets"])
-async def create_dataset():
-    return {"message": "This will create a new dataset"}
+# Retrieve a list of entries with optional skip and limit parameters
+@router.get("/entries/", response_model=List[schemas.EntryInDB], tags=["entries"])
+async def read_entries(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    entries = store.get_entries(db, skip=skip, limit=limit)
+    return entries
 
 
-@router.get("/entries/", tags=["entries"])
-async def read_entries():
-    return {"message": "This will return a list of entries"}
+# Retrieve a single entry by its ID
+@router.get("/entries/{entry_id}", response_model=schemas.EntryInDB, tags=["entries"])
+async def read_entry(entry_id: str, db: Session = Depends(get_db)):
+    entry = store.get_entry(db, entry_id=entry_id)
+    if entry is None:
+        raise HTTPException(status_code=404, detail="Entry not found")
+    return entry
 
 
-@router.get("/entries/{entry_id}", tags=["entries"])
-async def read_entry(entry_id: str):
-    return {"message": f"This will return entry {entry_id}"}
+# Create a new entry using data provided in the request body
+@router.post("/entries/", response_model=schemas.EntryInDB, tags=["entries"])
+async def create_entry(entry: schemas.EntryCreate, db: Session = Depends(get_db)):
+    db_entry = store.create_entry(db, entry=entry)
+    return db_entry
 
 
-@router.post("/entries/", tags=["entries"])
-async def create_entry():
-    return {"message": "This will create a new entry"}
+# Create a new dataset using data provided in the request body
+@router.post("/datasets/", response_model=schemas.DatasetInDB, tags=["datasets"])
+async def create_dataset(dataset: schemas.DatasetCreate, db: Session = Depends(get_db)):
+    db_dataset = store.create_dataset(db, dataset=dataset)
+    return db_dataset
